5d534dd985cb637fcd5f47145561586cd7ea05b0
nqtronix <nqtronix@gmx.net>
2021-02-06 14:47:11 +0100

    Reduce flash usage further (556 -> 518 byte)

--- rid.c
+++ rid.c
@@ -47,6 +47,12 @@ typedef union
 	uint32_t u32[1];
 	uint16_t u16[2];
 	uint8_t u8[4];
+	struct  
+	{
+		uint32_t		: 8;
+		uint32_t u16m	:16;
+		uint32_t		: 8;
+	};
 } uint32_u;
 
 
@@ -66,8 +72,6 @@ uint16_t rid_res_low (uint16_t adc_val, uint16_t res_high_14b)
 	if (adc_val >= 1000)
 		return UINT16_MAX;
 	
-	//return ((((uint32_t)adc_val<<10)/(ADC_MAX_VALUE-adc_val))*res_high_14b)>>10;
-	
 	return rid_res(adc_val, ADC_MAX_VALUE-adc_val, res_high_14b);
 }
 
@@ -76,14 +80,33 @@ uint16_t rid_res_high (uint16_t adc_val, uint16_t res_low_14b)
 	// prevent division through 0 edge case
 	if (adc_val <= 11)
 		return UINT16_MAX;
-
-	//return ((((ADC_MAX_VALUE-(uint32_t)adc_val)<<10)/adc_val)*res_low_14b)>>10;
 	
 	return rid_res(ADC_MAX_VALUE-adc_val, adc_val, res_low_14b);
 }
 
 uint16_t rid_res (uint16_t dividend_10b, uint16_t divisor_10b, uint16_t res_14b)
 {
+	// The initial formula is quite simple, but not efficiently calculated on a 8bit MCU without FPU
+	// 1) result = res_14b * (dividend_10b / divisor_10b)
+	
+	// A normal integer division would not work here because the quotient is close to "1" in most
+	// cases. To solve this, the resistance can be first multiplied with the dividend:
+	// 2) result = (res_14b * dividend_10b) / divisor_10b
+	
+	// However, if the resistance value is small (less than 10 bits) the division will add additional
+	// error. To solve this the dividend can be shifted left (= multiplied by 2^n) and after the
+	// division shifted right (= divided by 2^n).
+	// 3) result = ( ((res_14b * dividend_10b)<<n) / divisor_10b) >>n
+	
+	// On AVR8 MCUs shifting by multiple bits is not possible with a single instruction, so the
+	// compiler is forced to insert a software implementation which will increase code size. Instead
+	// of shifting the value, it is much more effective to to access the 2 center bits of a uint32_t
+	// directly.
 
-	return ((((uint32_t)dividend_10b*res_14b<<8)/divisor_10b))>>8;
+	uint32_u tmp;
+	tmp.u16m	= res_14b;
+	tmp.u		*= dividend_10b;
+	tmp.u		/= divisor_10b;
+	
+	return tmp.u16m;
 }
